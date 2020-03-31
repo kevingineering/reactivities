b5bc96dbfa12f2a07545f1832a87502ca8cd2bd7
kcharders <kcharders@gmail.com>
2020-03-31 17:05:16 -0500

    create-react-app

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,38 +1,48 @@
-using Microsoft.AspNetCore.Builder; //UseDeveloperExceptionPage, UseRouting, useAuthorization, UseEndpoints
+using Microsoft.AspNetCore.Builder; //IApplicationBuilder, UseDeveloperExceptionPage, UseRouting, useAuthorization, UseEndpoints
 using Microsoft.AspNetCore.Hosting; //IWebHostEnvironment
 using Microsoft.EntityFrameworkCore;  //UseSQLite
-using Microsoft.Extensions.Configuration; //GetConnectionString
+using Microsoft.Extensions.Configuration; //GetConnectionString, IConfiguration
 using Microsoft.Extensions.DependencyInjection; //IServiceCollection, AddDbContext, AddControlers
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting; //env
 
 namespace API
 {
   public class Startup
   {
-    //constructor - injects configuration - allows us to access various configuration settings
-    public Startup(Microsoft.Extensions.Configuration.IConfiguration configuration)
+    //constructor - injects configuration
+    public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
     }
 
     //represents key/value pair 
-    public Microsoft.Extensions.Configuration.IConfiguration Configuration { get; }
+    public IConfiguration Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container (dependency injection)
-    public void ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection services)
+    public void ConfigureServices(IServiceCollection services)
     {
       //UseSqlite takes connection string from configuration files
       services.AddDbContext<Persistence.DataContext>(opt =>
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+
+      //add CORS headers - allow anything from localhost
+      services.AddCors(opt => 
+      {
+        opt.AddPolicy("CorsPolicy", policy=>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
+
       //adding controllers - previously AddMvc
       services.AddControllers();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     //can add middleware - order is important
-    public void Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       //if in development mode, provide detailed information in case of error
       if (env.IsDevelopment())
@@ -48,6 +58,9 @@ namespace API
 
       //for authentication
       app.UseAuthorization();
+
+      //add CORS
+      app.UseCors("CorsPolicy");
 
       //maps controller endpoints into API
       app.UseEndpoints(endpoints =>
